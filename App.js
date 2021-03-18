7ad6d389d02f0dadadd1f25d4625e219fdbf4b08
Weronika Janczuk <weronika.janczuk@gmail.com>
2021-03-18 12:00:43 -0500

    setup

--- App.js
+++ App.js
@@ -1,21 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import 'react-native-gesture-handler';
+import React, { useEffect, useState } from 'react';
+import { StatusBar } from 'react-native';
+import { firebase } from './src/firebase/config';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { LoginScreen, HomeScreen, RegistrationScreen } from './src/screens';
+import {decode, encode} from 'base-64'
+
+if (!global.btoa) {  global.btoa = encode }
+if (!global.atob) { global.atob = decode }
+
+const AppBase = createStackNavigator();
 
 export default function App() {
+
+  const [loading, setLoading] = useState(true)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    const usersRef = firebase.firestore().collection('users');
+    firebase.auth().onAuthStateChanged(user => {
+      if (user) {
+        usersRef
+          .doc(user.uid)
+          .get()
+          .then((document) => {
+            const userData = document.data()
+            setLoading(false)
+            setUser(userData)
+          })
+          .catch((error) => {
+            setLoading(false)
+          });
+      } else {
+        setLoading(false)
+      }
+    });
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar hidden = {false} backgroundColor = "#00BCD4" translucent = {true}/>
+      <AppBase.Navigator initialRouteName={user ? 'Home' : 'Login'} screenOptions={{headerShown: false}}>
+          <AppBase.Screen name='Home'>
+            {(props) => <HomeScreen {...props} extraData={user} />}
+          </AppBase.Screen>
+          <AppBase.Screen name='Login' component={LoginScreen} />
+          <AppBase.Screen name='Registration' component={RegistrationScreen} />
+      </AppBase.Navigator>
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
