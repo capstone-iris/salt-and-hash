723adc5d8dd780232d23f31296b2bd3567b88cad
Kathleen Tran <kathleen1.tran@gmail.com>
2021-03-23 15:06:32 -0400

    FE: component for single event that invited user will see

---
+++ src/screens/AllEventsIndex/SingleEventScreen/styles.js
@@ -0,0 +1,46 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#eee1db',
+		color: 'black',
+	},
+	menuWrapper: {
+		marginTop: 10,
+	},
+	menuItem: {
+		flexDirection: 'row',
+		paddingVertical: 15,
+		paddingHorizontal: 30,
+	},
+	menuItemText: {
+		color: '#777777',
+		marginLeft: 20,
+		fontWeight: '600',
+		fontSize: 16,
+		lineHeight: 26,
+	},
+	imageContainer: {
+		flex: 1,
+		flexDirection: 'row',
+		flexWrap: 'wrap',
+		justifyContent: 'space-around',
+		margin: 10,
+		marginTop: 20,
+		height: 850,
+	},
+	image: {
+		height: 100,
+		width: 100,
+		marginBottom: 20,
+		borderRadius: 15,
+	},
+	eventNameText: {
+		color: 'black',
+		fontWeight: '600',
+		fontSize: 25,
+		textAlign: 'center',
+		marginTop: 25,
+	},
+});
