728a8971f3de45573d18128b9599295cf5d14338
Kathleen Tran <kathleen1.tran@gmail.com>
2021-03-24 17:02:28 -0400

    trying to user data

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { SafeAreaView, View } from 'react-native';
 import styles from './styles';
 import { firebase } from '../../firebase/config';
@@ -12,8 +12,64 @@ import {
 
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-export default function ProfileScreen({ navigation }) {
-	const onSignOut = () => {
+export default class ProfileScreen extends Component {
+	constructor() {
+		super();
+		this.state = {
+			users: {},
+		};
+	}
+
+	componentDidMount() {
+		firebase
+			.database()
+			.ref('users')
+			.on('value', (querySnapShot) => {
+				let data = querySnapShot.val() ? querySnapShot.val() : {};
+				let usersItems = { ...data };
+				this.setState({ users: usersItems });
+			});
+	}
+
+	// const data = firebase
+	// 	.firestore()
+	// 	.collection('users')
+	// 	.get()
+	// 	.then((snapshot) => {
+	// 		snapshot.docs.forEach((doc) => {
+	// 			doc;
+	// 		});
+	// 	});
+
+	// console.log(data);
+
+	// getUsers = () => {
+	// 	const usersRef = firebase.firestore().collection('users');
+
+	// 	usersRef.get().then((snapshot) => {
+	// 		snapshot.docs.forEach((doc) => {
+	// 			console.log(doc.data());
+	// 		});
+	// 	});
+	// };
+
+	// getUsers();
+
+	// const usersCollection = firebase.database().ref('users');
+	// usersCollection.on('value', (snapshot) => {
+	// 	const data = snapshot.val();
+	// 	console.log(snapshot);
+	// });
+
+	// const [email, setEmail] = React.useState('');
+
+	// React.useEffect(() => {
+	// 	let userEmail = firebase.auth().currentUser.email;
+	// 	setEmail(userEmail);
+	// 	console.log('User email is', userEmail);
+	// }, []);
+
+	onSignOut = () => {
 		firebase
 			.auth()
 			.signOut()
@@ -25,96 +81,99 @@ export default function ProfileScreen({ navigation }) {
 			});
 	};
 
-	return (
-		<SafeAreaView style={styles.container}>
-			<View style={styles.userInfoSection}>
-				<View
-					style={{
-						flexDirection: 'row',
-						marginTop: 30,
-						alignItems: 'center',
-					}}
-				>
-					<Avatar.Image
-						source={{
-							uri:
-								'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUTBClYADFs5Xv7s0Uu3_1eXFalW-VzKMp74KcMmwDuYJwGdyeFpSSNB6x1w&usqp=CAc',
+	render() {
+		console.log(this.state);
+		return (
+			<SafeAreaView style={styles.container}>
+				<View style={styles.userInfoSection}>
+					<View
+						style={{
+							flexDirection: 'row',
+							marginTop: 30,
+							alignItems: 'center',
 						}}
-						size={80}
-					/>
-					<View style={{ marginLeft: 20 }}>
-						<Title style={styles.title}>John Doe</Title>
-						<Caption style={styles.caption}>@j_doe</Caption>
+					>
+						<Avatar.Image
+							source={{
+								uri:
+									'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTUTBClYADFs5Xv7s0Uu3_1eXFalW-VzKMp74KcMmwDuYJwGdyeFpSSNB6x1w&usqp=CAc',
+							}}
+							size={80}
+						/>
+						<View style={{ marginLeft: 20 }}>
+							<Title style={styles.title}>John Doe</Title>
+							<Caption style={styles.caption}>@j_doe</Caption>
+						</View>
 					</View>
 				</View>
-			</View>
 
-			<View style={styles.userInfoSection}>
-				<View style={styles.row}>
-					<Icon name='map-marker-radius' size={20} color='#777777' />
-					<Text style={{ color: '#777777', marginLeft: 20 }}>
-						Fort Lauderdale, FL
-					</Text>
+				<View style={styles.userInfoSection}>
+					<View style={styles.row}>
+						<Icon name='map-marker-radius' size={20} color='#777777' />
+						<Text style={{ color: '#777777', marginLeft: 20 }}>
+							Fort Lauderdale, FL
+						</Text>
+					</View>
+					<View style={styles.row}>
+						<Icon name='phone' size={20} color='#777777' />
+						<Text style={{ color: '#777777', marginLeft: 20 }}>
+							+(850) 675-4567
+						</Text>
+					</View>
+					<View style={styles.row}>
+						<Icon name='email' size={20} color='#777777' />
+						<Text style={{ color: '#777777', marginLeft: 20 }}>
+							johndoe@email.com
+						</Text>
+					</View>
 				</View>
-				<View style={styles.row}>
-					<Icon name='phone' size={20} color='#777777' />
-					<Text style={{ color: '#777777', marginLeft: 20 }}>
-						+(850) 675-4567
-					</Text>
-				</View>
-				<View style={styles.row}>
-					<Icon name='email' size={20} color='#777777' />
-					<Text style={{ color: '#777777', marginLeft: 20 }}>
-						john_doe@email.com
-					</Text>
-				</View>
-			</View>
 
-			<View style={styles.infoBoxWrapper}>
-				<View style={styles.infoBox}>
-					<Title>1</Title>
-					<Caption>Hosted Events</Caption>
+				<View style={styles.infoBoxWrapper}>
+					<View style={styles.infoBox}>
+						<Title>1</Title>
+						<Caption>Hosted Events</Caption>
+					</View>
+					<View style={styles.infoBox}>
+						<Title>4</Title>
+						<Caption>Invited Events</Caption>
+					</View>
 				</View>
-				<View style={styles.infoBox}>
-					<Title>4</Title>
-					<Caption>Invited Events</Caption>
-				</View>
-			</View>
 
-			<View style={styles.menuWrapper}>
-				<TouchableRipple onPress={() => {}}>
-					<View style={styles.menuItem}>
-						<Icon name='heart-outline' color='#FF6347' size={25} />
-						<Text style={styles.menuItemText}>Your Favorites</Text>
-					</View>
-				</TouchableRipple>
-				<TouchableRipple onPress={() => {}}>
-					<View style={styles.menuItem}>
-						<Icon name='credit-card-outline' color='#FF6347' size={25} />
-						<Text style={styles.menuItemText}>Payment</Text>
-					</View>
-				</TouchableRipple>
-				<TouchableRipple onPress={() => {}}>
-					<View style={styles.menuItem}>
-						<Icon name='bell-outline' color='#FF6347' size={25} />
-						<Text style={styles.menuItemText}>Notifications</Text>
-					</View>
-				</TouchableRipple>
-				<TouchableRipple onPress={() => {}}>
-					<View style={styles.menuItem}>
-						<Icon name='account-check-outline' color='#FF6347' size={25} />
-						<Text style={styles.menuItemText}>Support</Text>
-					</View>
-				</TouchableRipple>
-				<TouchableRipple onPress={onSignOut}>
-					<View style={styles.menuItem}>
-						<Icon name='logout' color='#FF6347' size={25} />
-						<Text style={styles.menuItemText}>Sign out</Text>
-					</View>
-				</TouchableRipple>
-			</View>
-		</SafeAreaView>
-	);
+				<View style={styles.menuWrapper}>
+					<TouchableRipple onPress={() => {}}>
+						<View style={styles.menuItem}>
+							<Icon name='heart-outline' color='#FF6347' size={25} />
+							<Text style={styles.menuItemText}>Your Favorites</Text>
+						</View>
+					</TouchableRipple>
+					<TouchableRipple onPress={() => {}}>
+						<View style={styles.menuItem}>
+							<Icon name='credit-card-outline' color='#FF6347' size={25} />
+							<Text style={styles.menuItemText}>Payment</Text>
+						</View>
+					</TouchableRipple>
+					<TouchableRipple onPress={() => {}}>
+						<View style={styles.menuItem}>
+							<Icon name='bell-outline' color='#FF6347' size={25} />
+							<Text style={styles.menuItemText}>Notifications</Text>
+						</View>
+					</TouchableRipple>
+					<TouchableRipple onPress={() => {}}>
+						<View style={styles.menuItem}>
+							<Icon name='account-check-outline' color='#FF6347' size={25} />
+							<Text style={styles.menuItemText}>Support</Text>
+						</View>
+					</TouchableRipple>
+					<TouchableRipple onPress={this.onSignOut}>
+						<View style={styles.menuItem}>
+							<Icon name='logout' color='#FF6347' size={25} />
+							<Text style={styles.menuItemText}>Sign out</Text>
+						</View>
+					</TouchableRipple>
+				</View>
+			</SafeAreaView>
+		);
+	}
 }
 
 // import React from 'react';
