bbb2abdd732ee59037fc0e014fd02e0fe642c734
Weronika Janczuk <69174046+wjanczuk@users.noreply.github.com>
2021-03-25 16:26:39 -0500

    added the text + invite options

--- src/screens/RestaurantsIndex/EventRestaurantsScreen/styles.js
+++ src/screens/RestaurantsIndex/EventRestaurantsScreen/styles.js
@@ -52,5 +52,17 @@ export default StyleSheet.create({
 	voteContainer: {
 		flex: 1,
 		flexDirection: 'row'
+	},
+	input: {
+		height: 48,
+		borderRadius: 5,
+		overflow: 'hidden',
+		backgroundColor: 'white',
+		marginTop: 10,
+		marginBottom: 10,
+		marginLeft: 30,
+		marginRight: 30,
+		paddingLeft: 16,
+		width: 250,
 	}
 });
