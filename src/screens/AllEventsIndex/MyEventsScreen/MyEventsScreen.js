08f224fe27f662c1fae27d768d0d30ea6a413b67
Kathleen Tran <kathleen1.tran@gmail.com>
2021-03-27 02:22:05 -0400

    changed events tabs order and name, added an message in host tab if user does not have hosted events

--- src/screens/AllEventsIndex/MyEventsScreen/MyEventsScreen.js
+++ src/screens/AllEventsIndex/MyEventsScreen/MyEventsScreen.js
@@ -23,9 +23,9 @@ export default function MyEventsScreen({ navigation }) {
 
 	const [index, setIndex] = React.useState(0);
 	const [routes] = React.useState([
-		{ key: 'hosted', title: 'Hosted Events' },
-		{ key: 'invited', title: 'Invited Events' },
-		{ key: 'create', title: 'Create Event' },
+		{ key: 'create', title: 'CREATE' },
+		{ key: 'hosted', title: 'HOST' },
+		{ key: 'invited', title: 'ATTEND' },
 	]);
 
 	const renderScene = SceneMap({
