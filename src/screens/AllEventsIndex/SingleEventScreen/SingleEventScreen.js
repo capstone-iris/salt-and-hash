e32d7c914eee51b05a5a313dd42804e126d1dd21
Kathleen Tran <kathleen1.tran@gmail.com>
2021-04-03 16:49:43 -0400

    users now can see the selected restaurants once the deadline is up.

--- src/screens/AllEventsIndex/SingleEventScreen/SingleEventScreen.js
+++ src/screens/AllEventsIndex/SingleEventScreen/SingleEventScreen.js
@@ -6,21 +6,19 @@ import {
 	ScrollView,
 	Button,
 	TouchableOpacity,
-	Image
-
+	Image,
 } from 'react-native';
 import { Text, TouchableRipple } from 'react-native-paper';
 import { MaterialCommunityIcons, Ionicons } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
 import * as base from '../../../../secrets.js';
 import { firebase } from './../../../firebase/config';
-import AddGuestsToEventScreen from '../CreateEventIndex/AddGuestsToEventScreen/AddGuestsToEventScreen.js'
+import AddGuestsToEventScreen from '../CreateEventIndex/AddGuestsToEventScreen/AddGuestsToEventScreen.js';
 import { Alert } from 'react-native';
-import EventsHostedScreen from '../EventsHostedScreen/EventsHostedScreen'
+import EventsHostedScreen from '../EventsHostedScreen/EventsHostedScreen';
 
 export default function SingleEventScreen({ route }) {
 	const { event } = route.params;
-
 
 	const navigation = useNavigation();
 	const [restaurantsData, setRestaurantsData] = useState([]);
@@ -60,44 +58,44 @@ export default function SingleEventScreen({ route }) {
 			: time;
 	}
 
-
-	function fetchImage (photoRef)  {
-		const ref = photoRef
-			const url = 'https://maps.googleapis.com/maps/api/place/photo?';
-			const maxWidth = '&maxwidth=600';
-			const photoReference = `&photoreference=${ref}`;
-			const key = `&key=${base.GOOGLE_PLACES_API}`; //insert key here
-			const fetchImageUrl = url + maxWidth + photoReference + key;
-			return fetchImageUrl;
-		};
-
-	const deleteAlert=()=>
-	Alert.alert(
-		"Are you sure you want to delete this event?",
-		" ",
-		[
-			{
-				text: "Cancel",
-				onPress: () => console.log("Cancel Pressed"),
-				style: "cancel"
-			},
-			{ text: "OK", onPress: () => deleteEvent()}
-		]
-	);
-
-	function deleteEvent(){
-		firebase.firestore().collection('events').doc(event.docId).delete()
-		navigation.navigate('My Events')
-
+	function fetchImage(photoRef) {
+		const ref = photoRef;
+		const url = 'https://maps.googleapis.com/maps/api/place/photo?';
+		const maxWidth = '&maxwidth=600';
+		const photoReference = `&photoreference=${ref}`;
+		const key = `&key=${base.GOOGLE_PLACES_API}`; //insert key here
+		const fetchImageUrl = url + maxWidth + photoReference + key;
+		return fetchImageUrl;
 	}
 
+	const deleteAlert = () =>
+		Alert.alert('Are you sure you want to delete this event?', ' ', [
+			{
+				text: 'Cancel',
+				onPress: () => console.log('Cancel Pressed'),
+				style: 'cancel',
+			},
+			{ text: 'OK', onPress: () => deleteEvent() },
+		]);
 
+	function deleteEvent() {
+		firebase.firestore().collection('events').doc(event.docId).delete();
+		navigation.navigate('My Events');
+	}
+
+	function isDeadLine(date) {
+		const today = new Date();
+		const deadline = new Date(date);
+		if (today.getTime() > deadline.getTime()) {
+			return true;
+		}
+		return false;
+	}
 
 	useEffect(() => {
-
 		// async function fetchData() {
-		console.log('in fetch data')
-			const unsubscribe = firebase
+		console.log('in fetch data');
+		const unsubscribe = firebase
 			.firestore()
 			.collection('eventRestaurants')
 			.doc(event.docId)
@@ -105,21 +103,30 @@ export default function SingleEventScreen({ route }) {
 			.onSnapshot((snapshot) => {
 				const result = [];
 				snapshot.forEach((doc) => {
-				result.push(doc.data());
+					result.push(doc.data());
+				});
+				setRestaurantsData(result);
 			});
-			setRestaurantsData(result);
-			});
-			return () => unsubscribe();
+		return () => unsubscribe();
 	}, []);
 
+	const maxVotes = restaurantsData.length
+		? restaurantsData.reduce((prev, current) =>
+				prev.votes > current.votes ? prev : current
+		  )
+		: {};
 
 	return (
 		<SafeAreaView style={styles.container}>
 			<Text style={styles.eventNameText}>{event.name}</Text>
-			<View style={{marginBottom: 35}}>
+			<View style={{ marginBottom: 35 }}>
 				<View>
 					<View style={styles.menuItem}>
-						<MaterialCommunityIcons name='calendar-range' color='#FF6347' size={25} />
+						<MaterialCommunityIcons
+							name='calendar-range'
+							color='#FF6347'
+							size={25}
+						/>
 						<Text style={styles.menuItemText}>
 							{convertDateTime(event.date.seconds, 'date')}
 						</Text>
@@ -127,7 +134,11 @@ export default function SingleEventScreen({ route }) {
 				</View>
 				<View>
 					<View style={styles.menuItem}>
-						<MaterialCommunityIcons name='clock-outline' color='#FF6347' size={25} />
+						<MaterialCommunityIcons
+							name='clock-outline'
+							color='#FF6347'
+							size={25}
+						/>
 						<Text style={styles.menuItemText}>
 							{convertDateTime(event.eventStartTime.seconds, 'time')} -{' '}
 							{convertDateTime(event.eventEndTime.seconds, 'time')}
@@ -136,7 +147,11 @@ export default function SingleEventScreen({ route }) {
 				</View>
 				<View>
 					<View style={styles.menuItem}>
-						<MaterialCommunityIcons name='timer-sand' color='#FF6347' size={25} />
+						<MaterialCommunityIcons
+							name='timer-sand'
+							color='#FF6347'
+							size={25}
+						/>
 						<Text style={styles.menuItemText}>
 							Voting Deadline:{' '}
 							{convertDateTime(event.votingDeadline.seconds, 'date')}
@@ -151,40 +166,50 @@ export default function SingleEventScreen({ route }) {
 				</View>
 				<View>
 					<TouchableRipple onPress={() => {}}>
-					<View style={styles.menuItem}>
-						<MaterialCommunityIcons name='map-marker-radius' color='#FF6347' size={25} />
-						<Text style={styles.menuItemText}>Final Restaurant Pending...</Text>
-					</View>
-
+						<View style={styles.menuItem}>
+							<MaterialCommunityIcons
+								name='map-marker-radius'
+								color='#FF6347'
+								size={25}
+							/>
+							<Text style={styles.menuItemText}>
+								{isDeadLine(
+									convertDateTime(event.votingDeadline.seconds, 'date')
+								)
+									? maxVotes.name
+									: 'Event location is Pending...'}
+							</Text>
+						</View>
 					</TouchableRipple>
 				</View>
 				<View>
-					{currentUser === event.userId ?
-						<View style={{marginTop: 15, marginBottom: 25}}>
-						<AddGuestsToEventScreen eventId={event.docId}/>
+					{currentUser === event.userId ? (
+						<View style={{ marginTop: 15, marginBottom: 25 }}>
+							<AddGuestsToEventScreen eventId={event.docId} />
 						</View>
-						: 
-						null
-					}
+					) : null}
 				</View>
-				</View>
+			</View>
 
+			{currentUser === event.userId ? (
+				<Button onPress={deleteAlert} title='Delete Event'></Button>
+			) : null}
 
-				{currentUser === event.userId ?
-					<Button onPress={deleteAlert} title='Delete Event'>
-					</Button>
-				: null
-				}
-
-				<ScrollView>
+			<ScrollView>
 				<View style={styles.restaurantsContainer}>
-						{restaurantsData.map((restaurant, index) => {
-							 return (
-								<View style={styles.indRestaurantContainer}>
+					{restaurantsData.map((restaurant, index) => {
+						return (
+							<View style={styles.indRestaurantContainer}>
 								<TouchableOpacity
 									activeOpacity={0.5}
 									key={index}
-									onPress={() => navigation.navigate('Restaurant Swipe', {restaurantsData: restaurantsData, eventId:event.docId, navigation: navigation})}
+									onPress={() =>
+										navigation.navigate('Restaurant Swipe', {
+											restaurantsData: restaurantsData,
+											eventId: event.docId,
+											navigation: navigation,
+										})
+									}
 								>
 									<Image
 										style={styles.image}
@@ -197,11 +222,11 @@ export default function SingleEventScreen({ route }) {
 									<Text style={styles.restaurantTitle}>{restaurant.name}</Text>
 									<Text style={styles.voteText}>{restaurant.votes} Votes</Text>
 								</View>
-								</View>
-						)})}
+							</View>
+						);
+					})}
 				</View>
-				</ScrollView>
-				
+			</ScrollView>
 		</SafeAreaView>
 	);
 }
